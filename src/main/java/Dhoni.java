5cc5285201badbbc0932b305ccf74900fe424b9e
AbhishekParanjape <abhishekrparanjape@gmail.com>
2026-01-22 02:06:02 +0800

    Rename, Greet, Exit.

--- src/main/java/Dhoni.java
+++ src/main/java/Dhoni.java
@@ -1,10 +1,12 @@
 public class Dhoni {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Dhoni";
+        String line = "-----------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
