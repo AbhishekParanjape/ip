8efa35856d2569cc31175789820eefa39fc47424
AbhishekParanjape <abhishekrparanjape@gmail.com>
2026-01-22 15:52:05 +0800

    Echo commands and exit with bye.

--- src/main/java/Dhoni.java
+++ src/main/java/Dhoni.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
+
 public class Dhoni {
+    private static void echo(String text) {
+        String line = "-----------------------------------------";
+        System.out.println("\t" + line);
+        System.out.println("\t" + text);
+        System.out.println("\t" + line);
+    }
+
     public static void main(String[] args) {
         String name = "Dhoni";
         String line = "-----------------------------------------";
-        System.out.println(line);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println("\t" + line);
+        System.out.println("\t" + "Hello! I'm " + name);
+        System.out.println("\t" + "What can I do for you?");
+        System.out.println("\t" + line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            echo(userInput);
+            userInput = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println("\t" + line);
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
+        System.out.println("\t" + line);
+
     }
 }
+
